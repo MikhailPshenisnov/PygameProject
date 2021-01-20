f499be49cd1a1f12bd1a82f1fa84e238848d3940
MikhailPshenisnov <mishail.1703@gmail.com>
2021-01-21 00:00:27 +0300

    Новая иконка в формате ico

--- Game/BottleField 2.py
+++ Game/BottleField 2.py
@@ -545,7 +545,7 @@ pygame.init()
 
 # Создание окна
 size = width, height = 1280, 720
-pygame.display.set_icon(pygame.image.load("data/PngFiles/Other/Small_Icon.png"))
+pygame.display.set_icon(pygame.image.load("data/PngFiles/Other/Icon.ico"))
 pygame.display.set_caption("BottleField 2")
 screen = pygame.display.set_mode(size)
 
